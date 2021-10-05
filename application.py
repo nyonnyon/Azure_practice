554f3a3423344e1bab3de155091b2dac460485ae
nyonnyon <ryukiseed0920@gmail.com>
2021-10-06 00:50:34 +0900

    intial commit
    My first git commit .

---
+++ application.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+
+@app.route('/profile/<string:user_name>')
+def profile(user_name):
+    return '{name}さんのプロフィール画面です'.format(name=user_name)
+
+
+
+if __name__ == '__main__':
+    app.run()
